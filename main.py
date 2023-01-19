15b5bc82f21896a106d5572ffbeabb162aea5f1f
amay.advani <amayadvani@gmail.com>
2023-01-19 12:07:06 -0800

    monkey done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from saakd_api.api.todo import todo_bp
 from saakd_api.api.calculator import calculator_bp
 from saakd_api.api.timer import timer_bp
 from saakd_api.api.note import note_bp
+from saakd_api.model.notes import init_notes
 
 app.register_blueprint(todo_bp)
 app.register_blueprint(timer_bp)
@@ -15,7 +16,7 @@ app.register_blueprint(calculator_bp)
 def init_db():
     with app.app_context():
         db.create_all()
-
+        init_notes()
 
 if __name__ == "__main__":
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./volumes/sqlite.db"
--- saakd_api/model/notes.py
+++ saakd_api/model/notes.py
@@ -9,9 +9,9 @@ class Notes(db.Model):
     _subject = Column(String(225), nullable=False)
     _text = Column(String(1000), nullable=False)
 
-    def __init__(self, text):
+    def __init__(self, text, subject):
         self._text = text
-        self._subject = True
+        self._subject = subject
 
     def __repr__(self):
         return "<Notes(id='%s', text='%s', subject='%s')>" % (
@@ -38,3 +38,18 @@ class Notes(db.Model):
 
     def to_dict(self):
         return {"id": self.id, "text": self.text, "subject": self.subject}
+
+
+def init_notes():
+    note = Notes("work on calc", "calc")
+    note2 = Notes("work on bio", "bio")
+    note3 = Notes("work on poe", "poe")
+    note4 = Notes("work on csp", "csp")
+    note5 = Notes("work on ush", "ush")
+    db.session.add(note)
+    db.session.add(note2)
+    db.session.add(note2)
+    db.session.add(note4)
+    db.session.add(note5)
+
+    db.session.commit()
