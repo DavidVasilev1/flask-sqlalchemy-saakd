591ec54d2f9ce8ccfded3f28f1222d3e619d751f
DavidVasilev1 <davidkvasilev@gmail.com>
2023-01-22 20:08:10 -0800

    fix

--- saakd_api/model/timers.py
+++ saakd_api/model/timers.py
@@ -5,10 +5,11 @@ from .. import db
 class Timer(db.Model):
     # defining table
     __tablename__ = "timer"
+
     id = Column(Integer, primary_key=True)
     _task = Column(String(255), nullable=False)
     _expectedtime = Column(Integer, nullable=False)
-    _started = Column(Boolean, nullable=False)
+    _started = Column(Integer, nullable=False)
     _timeStop = Column(Integer, nullable=False)
 
     # initialization
@@ -19,8 +20,9 @@ class Timer(db.Model):
         self._timeStop = timeStop
 
     def __repr__(self):
-        return "<Timer(id='%s', expectedtime='%s', started='%s', timeStop='%s')>" % (
+        return "<Timer(id='%s', self='%s', expectedtime='%s', started='%s', timeStop='%s')>" % (
             self.id,
+            self.task,
             self.expectedtime,
             self.started,
             self.timeStop
@@ -33,10 +35,6 @@ class Timer(db.Model):
     @task.setter
     def text(self, task):
         self._text = task
-
-    @property
-    def uuid(self):
-        return self._uuid
 
     @property
     def started(self):
@@ -65,12 +63,12 @@ class Timer(db.Model):
     def to_dict(self):
         return {"id": self.id, "task": self.task, "expectedtime": self.expectedtime, "started": self.started, "timeStop": self.timeStop}
 
-def init_timers():
-    task1 = Timer(task="math", expectedtime="58", started="True", timeStop="367")
-    task2 = Timer(task="physics", expectedtime="347", started="True", timeStop="56")
-    task3 = Timer(task="history", expectedtime="23", started="True", timeStop="678")
-    task4 = Timer(task="csp", expectedtime="56", started="False", timeStop="23")
-    task5 = Timer(task="english", expectedtime="89", started="True", timeStop="45")
+def initTimers():
+    task1 = Timer(task='math', expectedtime=58, started=1, timeStop=367)
+    task2 = Timer(task='physics', expectedtime=347, started=1, timeStop=56)
+    task3 = Timer(task='history', expectedtime=23, started=1, timeStop=678)
+    task4 = Timer(task='csp', expectedtime=56, started=0, timeStop=23)
+    task5 = Timer(task='english', expectedtime=89, started=1, timeStop=45)
     db.session.add(task1)
     db.session.add(task2)
     db.session.add(task3)
