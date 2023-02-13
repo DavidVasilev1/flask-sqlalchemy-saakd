46e33d017b54c60ce2cd85ab7f3431da1311330e
AlexKumar19 <alex.k.kumar@gmail.com>
2023-02-13 13:59:34 -0800

    Merge branch 'master' of https://github.com/safinsingh/flask-sqlalchemy-saakd

--- saakd_api/model/timers.py
+++ saakd_api/model/timers.py
@@ -19,17 +19,11 @@ class Timer(db.Model):
         self._TimeExpected = TimeExpected
 
     def __repr__(self):
-<<<<<<< HEAD
         return "<Timer(id='%s', storedtime='%s', tasks='%s', TimeExpected='%s')>" % (
             self.id,
             self.storedtime,
             self.tasks,
             self.TimeExpected,
-=======
-        return (
-            "<Timer(id='%s', self='%s', expectedtime='%s', started='%s', timeStop='%s')>"
-            % (self.id, self.task, self.expectedtime, self.started, self.timeStop)
->>>>>>> 3e25d5ed501bdb6d715708f7b53937f19088e816
         )
 
     @property
@@ -57,7 +51,6 @@ class Timer(db.Model):
         self.storedtime = value
 
     def to_dict(self):
-<<<<<<< HEAD
         return {"id": self.id, "tasks": self.tasks, "TimeExpected": self.TimeExpected, "storedtime": self.storedtime}
 
 
@@ -67,23 +60,6 @@ def initTimers():
     task3 = Timer(tasks='history', TimeExpected=23, storedtime=76)
     task4 = Timer(tasks='csp', TimeExpected=56, storedtime=65)
     task5 = Timer(tasks='english', TimeExpected=89, storedtime=100)
-=======
-        return {
-            "id": self.id,
-            "task": self.task,
-            "expectedtime": self.expectedtime,
-            "started": self.started,
-            "timeStop": self.timeStop,
-        }
-
-
-def init_timers():
-    task1 = Timer(task="math", expectedtime=58, started=1, timeStop=367)
-    task2 = Timer(task="physics", expectedtime=347, started=1, timeStop=56)
-    task3 = Timer(task="history", expectedtime=23, started=1, timeStop=678)
-    task4 = Timer(task="csp", expectedtime=56, started=0, timeStop=23)
-    task5 = Timer(task="english", expectedtime=89, started=1, timeStop=45)
->>>>>>> 3e25d5ed501bdb6d715708f7b53937f19088e816
     db.session.add(task1)
     db.session.add(task2)
     db.session.add(task3)
